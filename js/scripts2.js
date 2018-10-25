bf83ee86a397fad3b33f73ac21a01e62f8bdcdd6
Christopher Cahill and Victoria Martinez <victoria@>
2018-10-25 13:17:31 -0700

    works

--- js/scripts2.js
+++ js/scripts2.js
@@ -4,46 +4,37 @@ function howBig (inputNo) {
     var tooBig = ("that number is too big for Roman Numeral counting");
     return tooBig;
   }
-  else if (inputNo <= 3999) {
+  else if (inputNo <= 3999 && inputNo >= 1000) {
     var inputArray = inputNo.split('');
     var resultArray = [];
-    for (var i = 0; i < (arrayNumerals.length+1) ; i++) {
-        if (parseInt(inputArray[3]) === i) {
-        return (arrayNumerals[i-1]);
+    for (var i = 0; i < (arrayNumerals.length+1); i++) {
+      if (parseInt(inputArray[3]) === i) {
+        var resultArrayPos4 = arrayNumerals[i-1];
       }
-
-
-
-
-
     }
-    return resultArray;
+    for (var i = 0; i < (arrayNumTens.length+1); i++) {
+      if (parseInt(inputArray[2]) === i) {
+        var resultArrayPos3 = arrayNumTens[i-1];
+      }
+    }
+    for (var i = 0; i < (arrayHundreds.length+1); i++) {
+      if (parseInt(inputArray[1]) === i) {
+        var resultArrayPos2 = arrayHundreds[i-1];
+      }
+    }
+    for (var i = 0; i < (arrayThousands.length+1); i++) {
+      if (parseInt(inputArray[0]) === i) {
+        var resultArrayPos1 = arrayThousands[i-1];
+      }
+    }
   }
-
-  // else if (inputNo > 1000 && inputNo <=3999) {
-  //   var splitInput4 = inputNo.split('');
-  //   return(arrayThousands[splitInput4[0]-1].concat((arrayHundreds[splitInput4[1]-1]).concat((arrayNumTens[splitInput4[2]-1]).concat(arrayNumerals[splitInput4[3]-1]))));
-  // }
-  //
-  // else if (inputNo > 100 && inputNo <= 999) {
-  //   var splitInput3 = inputNo.split('');
-  //   return (arrayHundreds[splitInput3[0]-1]).concat((arrayNumTens[splitInput3[1]-1]).concat(arrayNumerals[splitInput3[2]-1]));
-  // }
-  // else if (inputNo >= 10 && inputNo <= 99) {
-  //   var splitInput2 = inputNo.split('');
-  //   return (arrayNumTens[splitInput2[0]-1]).concat(arrayNumerals[splitInput2[1]-1]);
-  // }
-  // else if (inputNo <10) {
-  //   var onesNo = inputNo % 10;
-  //   return arrayNumerals[(onesNo)-1];
-  // }
+    return resultArrayPos1+resultArrayPos2+resultArrayPos3+resultArrayPos4;
 }
 
 var arrayNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 var arrayNumTens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"];
 var arrayHundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
 var arrayThousands = ["M", "MM", "MMM"];
-
 
 $(document).ready(function(){
   $("#inputRomanNumeral").submit(function(event){
@@ -56,7 +47,7 @@ $(document).ready(function(){
     $(".resultDiv").empty();
     $(".resultDiv").append(result);
 
-    console.log(result);
+    // console.log(result);
 
   });
 });
