48ed59bd3e709abd17cb14bd912bc34dc8ace6d4
Christopher Cahill and Victoria Martinez <victoria@>
2018-10-25 14:56:07 -0700

    works both ways and code updated

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,29 @@ function howBig (inputNo) {
     var tooBig = ("that number is too big for Roman Numeral counting");
     return tooBig;
   }
-  else if (inputNo > 1000 && inputNo <=3999) {
+  else if (inputNo >= 1000 && inputNo <=3999) {
     var splitInput4 = inputNo.split('');
-    return(arrayThousands[splitInput4[0]-1].concat((arrayHundreds[splitInput4[1]-1]).concat((arrayNumTens[splitInput4[2]-1]).concat(arrayNumerals[splitInput4[3]-1]))));
+    return(arrayThousands[splitInput4[0]].concat((arrayHundreds[splitInput4[1]]).concat((arrayNumTens[splitInput4[2]]).concat(arrayNumerals[splitInput4[3]]))));
   }
-
-  else if (inputNo > 100 && inputNo <= 999) {
+  else if (inputNo >= 100 && inputNo <= 999) {
+    debugger
     var splitInput3 = inputNo.split('');
-    return (arrayHundreds[splitInput3[0]-1]).concat((arrayNumTens[splitInput3[1]-1]).concat(arrayNumerals[splitInput3[2]-1]));
+    return (arrayHundreds[splitInput3[0]]).concat((arrayNumTens[splitInput3[1]]).concat(arrayNumerals[splitInput3[2]]));
   }
   else if (inputNo >= 10 && inputNo <= 99) {
     var splitInput2 = inputNo.split('');
-    return (arrayNumTens[splitInput2[0]-1]).concat(arrayNumerals[splitInput2[1]-1]);
+    return (arrayNumTens[splitInput2[0]]).concat(arrayNumerals[splitInput2[1]]);
   }
-  else if (inputNo <10) {
-    var onesNo = inputNo % 10;
-    return arrayNumerals[(onesNo)-1];
+  else if (inputNo < 10) {
+    // var onesNo = inputNo % 10;
+    return arrayNumerals[(inputNo)];
   }
 }
 
-var arrayNumerals = ["I", "I", "I", "IV", "V", "VI", "VI", "VI", "IX",];
-var arrayNumTens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"];
-var arrayHundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-var arrayThousands = ["M", "MM", "MMM"];
+var arrayNumerals = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX",];
+var arrayNumTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"];
+var arrayHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+var arrayThousands = ["", "M", "MM", "MMM"];
 
 
 $(document).ready(function(){
--- js/scripts2.js
+++ js/scripts2.js
@@ -6,40 +6,107 @@ function howBig (inputNo) {
   }
   else if (inputNo <= 3999 && inputNo >= 1000) {
     var inputArray = inputNo.split('');
-    var resultArray = [];
-    for (var i = 0; i < (arrayNumerals.length+1); i++) {
-      if (parseInt(inputArray[3]) === i) {
-        var resultArrayPos4 = arrayNumerals[i-1];
+    for (var i = 0; i < arrayNumerals.length; i++) {
+      if (parseInt(inputArray[3]) === i ) {
+        var resultArrayPos4 = arrayNumerals[i];
+        }
+    }
+    for (var i = 0; i < arrayNumTens.length; i++) {
+      if (parseInt(inputArray[2]) === i) {
+        var resultArrayPos3 = arrayNumTens[i];
       }
     }
+    for (var i = 0; i < arrayHundreds.length; i++) {
+      if (parseInt(inputArray[1]) === i) {
+        var resultArrayPos2 = arrayHundreds[i];
+      }
+    }
+    for (var i = 0; i < arrayThousands.length; i++) {
+      if (parseInt(inputArray[0]) === i) {
+        var resultArrayPos1 = arrayThousands[i];
+      }
+    }
+  }
+  else if (inputNo <= 999 && inputNo >= 100) {
+    var inputArray = inputNo.split('');
+    for (var i = 0; i < (arrayNumerals.length+1); i++) {
+      if (parseInt(inputArray[2]) === i ) {
+        var resultArrayPos4 = arrayNumerals[i];
+        }
+    }
     for (var i = 0; i < (arrayNumTens.length+1); i++) {
-      if (parseInt(inputArray[2]) === i) {
-        var resultArrayPos3 = arrayNumTens[i-1];
+      if (parseInt(inputArray[1]) === i) {
+        var resultArrayPos3 = arrayNumTens[i];
       }
     }
     for (var i = 0; i < (arrayHundreds.length+1); i++) {
-      if (parseInt(inputArray[1]) === i) {
-        var resultArrayPos2 = arrayHundreds[i-1];
+      if (parseInt(inputArray[0]) === i) {
+        var resultArrayPos2 = arrayHundreds[i];
+        var resultArrayPos1 = arrayThousands[0];
       }
     }
-    for (var i = 0; i < (arrayThousands.length+1); i++) {
+  }
+  else if (inputNo <= 99 && inputNo >= 10) {
+    var inputArray = inputNo.split('');
+    for (var i = 0; i < (arrayNumerals.length+1); i++) {
+      if (parseInt(inputArray[1]) === i ) {
+        var resultArrayPos4 = arrayNumerals[i];
+        }
+    }
+    for (var i = 0; i < (arrayNumTens.length+1); i++) {
       if (parseInt(inputArray[0]) === i) {
-        var resultArrayPos1 = arrayThousands[i-1];
+        var resultArrayPos3 = arrayNumTens[i];
+        var resultArrayPos1 = arrayThousands[0];
+        var resultArrayPos2 = arrayHundreds[0];
+      }
+    }
+  }
+  else if (inputNo <= 9 && inputNo >= 0) {
+    var inputArray = inputNo.split('');
+    var resultArrayPos1 = arrayThousands[0];
+    var resultArrayPos2 = arrayHundreds[0];
+    var resultArrayPos3 = arrayNumTens[0];
+    for (var i = 0; i < (arrayNumerals.length+1); i++) {
+      if (parseInt(inputArray[0]) === i ) {
+        var resultArrayPos4 = arrayNumerals[i];
+          }
+      else if (inputArray[0] === "0") {
+        var resultArrayPos4 = "you entered zero!";
       }
     }
   }
     return resultArrayPos1+resultArrayPos2+resultArrayPos3+resultArrayPos4;
 }
 
-var arrayNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-var arrayNumTens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"];
-var arrayHundreds = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-var arrayThousands = ["M", "MM", "MMM"];
+var arrayNumerals = ["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var arrayNumTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C"];
+var arrayHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+var arrayThousands = ["", "M", "MM", "MMM"];
 
 $(document).ready(function(){
   $("#inputRomanNumeral").submit(function(event){
     event.preventDefault();
 
+
+//     var minutesLabel = document.getElementById("minutes");
+//     var secondsLabel = document.getElementById("seconds");
+//     var totalSeconds = 0;
+//     setInterval(setTime, 1000);
+//
+//   function setTime() {
+//     ++totalSeconds;
+//     secondsLabel.innerHTML = pad(totalSeconds % 60);
+//     minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+// }
+//
+//   function pad(val) {
+//     var valString = val + "";
+//     if (valString.length < 2) {
+//       return "0" + valString;
+//     } else {
+//       return howBig(valString);
+//   }
+// }
 
     var inputNumber = $("#inputNumberField").val();
     var result = howBig(inputNumber);
@@ -47,7 +114,8 @@ $(document).ready(function(){
     $(".resultDiv").empty();
     $(".resultDiv").append(result);
 
-    // console.log(result);
+    console.log(result);
+    console.log(arrayNumerals.length);
 
   });
 });
